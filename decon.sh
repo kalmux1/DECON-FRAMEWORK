96f954c180ab4aa4635daf18ff7d289ba07a1a68
kalmux1 <kal.mux.cyber@gmail.com>
2024-02-25 19:26:56 +0530

    v1.0

--- decon.sh
+++ decon.sh
@@ -201,9 +201,9 @@ else
     {
         echo " "
         echo " "
-        echo -e "$BICyan      ENTER A VALID DOMAIN IN THE FORMAT {$Orange http://_______ | https://_______ $Normal}$Normal"
+        echo -e "$BICyan      ENTER A VALID DOMAIN IN THE FORMAT $Normal{$BIYellow http://_______ | https://_______$Normal $BICyan}$Normal"
         echo " "
-        read -e -p $'\033[1;93m      ========> \033[0m' domain
+        read -e -p $'\033[1;91m      ========> \033[0m' domain
         echo " "
         echo " "
         chars="/-\|"  
